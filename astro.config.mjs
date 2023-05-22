ea995ae2ace3b67a3de278aac41645c91297ea56
Subramaniam Srinivasan <subramaniam.s17@gmail.com>
2023-05-22 16:49:06 +0530

    Typo fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://subramaniam-s17.github.io",
-  base: "/onelot"
+  base: "/onelot",
   integrations: [
     tailwind(),
     image({
