209ba37284c15f1947b4053d3ec6b5ccbbce7c3d
Subramaniam Srinivasan <subramaniam.s17@gmail.com>
2023-05-22 16:47:29 +0530

    adding base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://subramaniam-s17.github.io",
+  base: "/onelot"
   integrations: [
     tailwind(),
     image({
